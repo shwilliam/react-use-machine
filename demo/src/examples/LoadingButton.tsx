c1d1dadb6b271cf8a688e10ae6bb2469c6fa023f
William L <w-lindvall@outlook.com>
2020-05-03 10:24:21 -0700

    update example to demo side effects

--- demo/src/examples/LoadingButton.tsx
+++ demo/src/examples/LoadingButton.tsx
@@ -1,52 +1,90 @@
-import React, {useEffect} from 'react'
+import React, {useState, SyntheticEvent} from 'react'
 import useMachine from '../dist'
 
+const fetchPokemon = (id: string): Promise<any> =>
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(d => d.json())
+
 const LoadingButton: React.FC = () => {
-  const {state, event, dispatch} = useMachine(
+  const [called, setCalled] = useState(0)
+  const [pokemonData, setPokemonData] = useState()
+  const [pokemonId, setPokemonId] = useState(1)
+
+  const {state, dispatch} = useMachine(
     {
       IDLE: {
-        DATA_REQUESTED: 'LOADING',
+        DATA_REQUESTED: {
+          to: 'LOADING',
+          effect: pokeId => {
+            fetchPokemon(pokeId)
+              .then(d => {
+                setPokemonData(d)
+                dispatch('DATA_SUCCESS')
+              })
+              .catch(() => {
+                dispatch('DATA_ERROR')
+              })
+              .finally(() => setCalled(s => s + 1))
+          },
+        },
       },
       LOADING: {
         DATA_SUCCESS: 'SUCCESS',
         DATA_ERROR: 'ERROR',
       },
       SUCCESS: {
-        DATA_REQUESTED: 'LOADING',
+        RESET: {
+          to: 'IDLE',
+          effect: () => setPokemonData(null),
+        },
       },
       ERROR: {
-        DATA_REQUESTED: 'LOADING',
+        RESET: 'IDLE',
       },
     },
     'IDLE',
   )
 
-  useEffect(() => {
-    switch (event) {
-      case 'DATA_REQUESTED':
-        fetch('https://swapi.co/api/people/1')
-          .then(d => d.json())
-          .then(d => {
-            console.log('data: ', d)
-            dispatch('DATA_SUCCESS')
-          })
-          .catch(e => {
-            console.log('err: ', e)
-            dispatch('DATA_ERROR')
-          })
-        break
-      default:
-        break
-    }
-  }, [event, dispatch])
+  const handlePokemonIdChange = (e: SyntheticEvent) => {
+    const target = e.target as HTMLInputElement
+    setPokemonId(Number(target.value))
+  }
+
+  const handlePokemonFormSubmit = (e: SyntheticEvent) => {
+    e.preventDefault()
+    dispatch({type: 'DATA_REQUESTED', payload: pokemonId})
+  }
 
   return (
-    <button
-      onClick={() => dispatch('DATA_REQUESTED')}
-      disabled={state === 'LOADING'}
-    >
-      load
-    </button>
+    <>
+      Called: {called} times
+      {state === 'SUCCESS' && (
+        <div>
+          <p>Name: {pokemonData.name}</p>
+          <p>Height: {pokemonData.height}</p>
+          <p>Weight: {pokemonData.weight}</p>
+        </div>
+      )}
+      {['IDLE', 'LOADING'].includes(state) && (
+        <form onSubmit={handlePokemonFormSubmit}>
+          <label>
+            Pokemon:{' '}
+            <input
+              type="number"
+              min="1"
+              value={pokemonId}
+              onChange={handlePokemonIdChange}
+            />
+          </label>
+
+          <button type="submit" disabled={state === 'LOADING'}>
+            load
+          </button>
+        </form>
+      )}
+      {['SUCCESS', 'ERROR'].includes(state) && (
+        <button onClick={() => dispatch('RESET')}>reset</button>
+      )}
+    </>
   )
 }
 
